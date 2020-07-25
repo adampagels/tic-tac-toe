2c2334bf86d4eb35c729a56822a40c46022a2d6f
adampagels <pagels@hotmail.ca>
2020-07-25 11:40:16 -0400

    Add moves to moveList array and display alert when a win condition has been met

--- index.js
+++ index.js
@@ -17,10 +17,11 @@ const gameBoard = (() => {
     }
   };
 
-  return { createGameBoard, resetGameBoard };
+  return { createGameBoard, resetGameBoard, board };
 })();
 
 const gameController = (() => {
+  const moveList = [];
   const winConditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -31,4 +32,15 @@ const gameController = (() => {
     [0, 4, 8],
     [2, 4, 6],
   ];
+
+  gameBoard.board.addEventListener("click", function (e) {
+    const index = Array.prototype.indexOf.call(this.children, e.target);
+    !moveList.includes(index) && moveList.push(index);
+    for (let i = 0; i < winConditions.length; i++) {
+      if (winConditions[i].every((elem) => moveList.indexOf(elem) > -1)) {
+        alert("you win");
+      }
+    }
+  });
+  gameBoard.createGameBoard();
 })();
